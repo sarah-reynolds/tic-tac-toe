111dec6d9cb46a79e1adb58bf4876baf52702ff3
Sarah Basinger <sarrey@gmail.com>
2016-12-08 10:54:52 -0500

    initial tictactoe board setup

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// 1. setup board
+// 2. user should be able to click a box and a mark shows up
+// 3. put an onclick on the first square
+// ---when user clicks, call functionthat putas x on the box
+// MILESTONE
+
+
+function markSquare(square){
+	console.log(square.id)
+}
